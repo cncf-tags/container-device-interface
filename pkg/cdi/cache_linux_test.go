693c957e3c57c82abb89e800a3fc3efe036254b9
Krisztian Litkey <krisztian.litkey@intel.com>
2025-02-24 11:48:32 +0200

    pkg/cdi: add test case for post-EMFILE recovery.
    Test that the cache indeed recovers functionally if the process
    ever recovers from not being able to create new file descriptors.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- pkg/cdi/cache_linux_test.go
+++ pkg/cdi/cache_linux_test.go
@@ -19,6 +19,7 @@ package cdi
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"syscall"
@@ -46,6 +47,100 @@ func TestTooManyOpenFiles(t *testing.T) {
 
 	// try to trigger original crash with a nil fsnotify.Watcher
 	_, _ = cache.InjectDevices(&oci.Spec{}, "vendor1.com/device=dev1")
+}
+
+func TestRecoveryAfterTooManyOpenFiles(t *testing.T) {
+	var (
+		etcDir = map[string]string{
+			"vendor1.yaml": `
+cdiVersion: "0.3.0"
+kind:       "vendor1.com/device"
+containerEdits:
+  env:
+  - VENDOR1_SPEC_VAR1=VAL1
+devices:
+  - name: "dev1"
+    containerEdits:
+      env:
+      - "VENDOR1_VAR1=VAL1"
+      deviceNodes:
+      - path: "/dev/vendor1-dev1"
+        type: b
+        major: 10
+        minor: 1
+`,
+		}
+
+		devices = []string{
+			"vendor1.com/device=dev1",
+		}
+
+		ociSpec = &oci.Spec{}
+
+		resultingSpec = &oci.Spec{
+			Process: &oci.Process{
+				Env: []string{
+					"VENDOR1_SPEC_VAR1=VAL1",
+					"VENDOR1_VAR1=VAL1",
+				},
+			},
+			Linux: &oci.Linux{
+				Devices: []oci.LinuxDevice{
+					{
+						Path:  "/dev/vendor1-dev1",
+						Type:  "b",
+						Major: 10,
+						Minor: 1,
+					},
+				},
+				Resources: &oci.LinuxResources{
+					Devices: []oci.LinuxDeviceCgroup{
+						{
+							Allow:  true,
+							Type:   "b",
+							Major:  int64ptr(10),
+							Minor:  int64ptr(1),
+							Access: "rwm",
+						},
+					},
+				},
+			},
+		}
+	)
+
+	dir, err := createSpecDirs(t, etcDir, nil)
+	require.NoError(t, err, "failed to create test directory")
+
+	// trigger EMFILE for fd creation: exhaust our file descriptor table
+	em, err := triggerEmfile()
+	require.NoError(t, err)
+	require.NotNil(t, em)
+	defer func() {
+		require.NoError(t, em.undo())
+	}()
+
+	_, err = syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, 0)
+	require.Equal(t, syscall.EMFILE, err)
+
+	cache := newCache(
+		WithSpecDirs(
+			filepath.Join(dir, "etc"),
+		),
+		WithAutoRefresh(true),
+	)
+	require.NotNil(t, cache)
+
+	// try to trigger original crash with a nil fsnotify.Watcher
+	_, _ = cache.InjectDevices(&oci.Spec{}, devices...)
+
+	// undo EMFILE for fd creation
+	require.NoError(t, em.undo())
+
+	// verify that injection works again
+	unresolved, err := cache.InjectDevices(ociSpec, devices...)
+	require.NoError(t, err)
+	require.Nil(t, unresolved)
+	require.Equal(t, resultingSpec, ociSpec)
 }
 
 type emfile struct {
