d78c80d84c1c8dff311157f5f699421389874136
Sebastiaan van Stijn <github@gone.nl>
2026-08-21 13:39:26 +0200

    specs-go: fix benchmark compatibility with go1.19
    The go.mod defines go1.19, which does not yet support b.Loop;
    
        # tags.cncf.io/container-device-interface/specs-go [tags.cncf.io/container-device-interface/specs-go.test]
        Error: ./version_bench_test.go:19:10: b.Loop undefined (type *testing.B has no field or method Loop)
        FAIL	tags.cncf.io/container-device-interface/specs-go [build failed]
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- specs-go/version_bench_test.go
+++ specs-go/version_bench_test.go
@@ -16,7 +16,7 @@ func BenchmarkValidateVersion(b *testing.B) {
 	for _, tc := range tests {
 		b.Run(tc.doc, func(b *testing.B) {
 			b.ReportAllocs()
-			for b.Loop() {
+			for i := 0; i < b.N; i++ {
 				_ = ValidateVersion(&Spec{Version: tc.version})
 			}
 		})
